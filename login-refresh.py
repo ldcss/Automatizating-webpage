1fa8cc7b7576535feb41970d0a580521a9586d05
Lucas Daniel Costa Da Silva <51211994+ldcss@users.noreply.github.com>
2022-08-05 22:35:40 -0300

    final version

--- login-refresh.py
+++ login-refresh.py
@@ -1,38 +1,57 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-import time
+from time import sleep
+from time import time
 
 def xpathWrite(xpath, key):
     elem = browser.find_element(By.XPATH, xpath)
     elem.clear()
     elem.send_keys(key)
 
-def xpathClick(xpath):
+def xpathClickNotRobot(xpath):
     elem = browser.find_element(By.XPATH, xpath)
     browser.execute_script("arguments[0].click();", elem)
     elem.send_keys(Keys.RETURN)
 
-def refreshMap(): #refresh the map every minute
-    t_end = time.time() + 20
-    while time.time() < t_end:
-        xpathClick('//i[@class="fa fa-globe fa-2x"]')
-        print('teste3')
+def xpathClick(xpath):
+    elem = browser.find_element(By.XPATH, xpath)
+    elem.click()
 
-while True:
-    #ABRE O BROWSER
-    browser = webdriver.Firefox()
-    browser.get("URL")
-    time.sleep(1)
-    original_window = browser.current_window_handle
-    xpathWrite("//input[@id='xxlogin']", "login")
-    xpathWrite("//input[@id='xxsenha']", "password")
-    #clicking in the no-robot button
-    xpathClick("//input[@class='is-checkradio has-background-color is-success']") 
-    time.sleep(2)
-    ###AFTER LOGIN
-    browser.get("URL_CLIENT_SESSION")
-    xpathClick('//input[@id="checkAll"]') #clicking on all clients
-    xpathClick('//input[@id="check2All"]') #clicking in all pages
+def loopRefresh(minLog, secRefresh):
+    minLog = minLog * 60
+    t_end = time() + minLog
+
+    while time() < t_end:
+        xpathClick('//i[@class="fa fa-globe fa-2x"]')
+        sleep(secRefresh) #espera 90s para clicar no botao
+
+def closingTabs():
+    for handle in browser.window_handles: #closing all tabs
+        browser.switch_to.window(handle)
+        browser.close() 
+
+if __name__ == "__main__":
+    while True:
+        browser = webdriver.Firefox()
+        browser.get("#URL-HERE")
+        sleep(1)
+        xpathWrite("//input[@id='xxlogin']", "#LOGIN-HERE")
+        xpathWrite("//input[@id='xxsenha']", "#PASSWORD-HERE")
+        xpathClickNotRobot("//input[@class='is-checkradio has-background-color is-success']") #Clicking not-robot button
+        sleep(2)
+        browser.get("http://www.arteempc.com.br/admin/clientes.php?tipo=todos")
+        xpathClick('//input[@id="checkAll"]')
+        xpathClickNotRobot('//input[@id="check2All"]') #accessing all clients
+        sleep(3)
+        xpathClick('//i[@class="fa fa-globe fa-2x"]') #showing all clients in maps
+        loopRefresh(0.5, 10)#get the minute for login time, then close the browser and login again. and getting the seconds for refreshing the map
+        closingTabs()
+
+
+    
+    
+    
+
+
+        
